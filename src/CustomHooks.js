511eedff26f11efc1f4b51807ff2101343b8c74b
tsubasahonda <lenonystalicelight+tsubasahonda@gmail.com>
2019-05-25 16:36:34 +0900

    useStateに関数を渡すところにコメント記述

--- src/CustomHooks.js
+++ src/CustomHooks.js
@@ -12,6 +12,7 @@ const useSignUpForm = (callback) => {
 
   const handleInputChange = (event) => {
     event.persist();
+    // https://scrapbox.io/onnanoko/React_Hooks%E3%81%A7%E3%83%95%E3%82%A9%E3%83%BC%E3%83%A0%E3%82%92%E4%BD%BF%E3%81%86
     setInputs(inputs => ({...inputs, [event.target.name]: event.target.value}));
   }
   return {
