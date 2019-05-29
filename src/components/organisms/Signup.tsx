2406a5358e726e279bec772e86d47184bf163471
tsubasahonda <lenonystalicelight+tsubasahonda@gmail.com>
2019-05-29 20:00:09 +0900

    Form作った

--- src/components/organisms/Signup.tsx
+++ src/components/organisms/Signup.tsx
@@ -1,41 +1,68 @@
 import * as React from 'react';
-import useSignUpForm from '../../stores/CustomHooks';
+import { useState, useCallback } from 'react';
 
-const Signup = () => {
-  const { inputs, handleInputChange, handleSubmit } = useSignUpForm();
-  return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>
-          First Name
-          <input type="text" name="firstName" onChange={handleInputChange} value={inputs.firstName} required />
-        </label>
-        <label>
-          Last Name
-          <input type="text" name="lastName" onChange={handleInputChange} value={inputs.lastName} required />
+type State = {
+  first_name: string
+  last_name: string
+  email: string
+  password1: string
+  password2: string
+};
+type Props = {
+  className?: string
+  handleSubmit?: (evnet: React.FormEvent<HTMLFormElement>) => void
+  onChangeInputText: (event: React.ChangeEvent<HTMLInputElement>) => void
+} & State;
+
+const Input: React.FC<Props> = (props: Props) => (
+  <label htmlFor={props[name]}>
+    <input
+      name={name}
+      type="text"
+      value={props[name]}
+      onChange={props.onChangeInputText}
+    />
+  </label>
+);
+
+const Component: React.FC<Props> = (props: Props) => (
+  <form className={props.className} onSubmit={props.handleSubmit}>
+    {(['first_name', 'last_name', 'email', 'password1', 'password2'] as const).map(name => (
+      <div key={name}>
+        <label htmlFor={props[name]}>
+          {name}
+          <input
+            name={name}
+            type="text"
+            value={props[name]}
+            onChange={props.onChangeInputText}
+          />
         </label>
       </div>
-      <div>
-        <label>
-          Email Address
-          <input type="email" name="email" onChange={handleInputChange} value={inputs.email} required />
-        </label>
-      </div>
-      <div>
-        <label>
-          Password
-          <input type="password" onChange={handleInputChange} value={inputs.password1} name="password1" />
-        </label>
-      </div>
-      <div>
-        <label>
-          Re-enter Password
-          <input type="password" onChange={handleInputChange} value={inputs.passwrod2} name="password2" />
-        </label>
-      </div>
-      <button type="submit">Sign Up</button>
-    </form>
+    ))}
+    <button type="submit">Sign Up</button>
+  </form>
+);
+
+const Container: React.FC = () => {
+  const [state, update] = useState<State>({
+    first_name: '',
+    last_name: '',
+    email: '',
+    password1: '',
+    password2: '',
+  });
+  const onChangeInputText = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      event.persist();
+      update(prev => ({
+        ...prev,
+        [event.target.name]: event.target.value,
+      }));
+    },
+    [],
   );
+  return <Component {...state} onChangeInputText={onChangeInputText} />;
 };
 
-export default Signup;
+export default Container;
